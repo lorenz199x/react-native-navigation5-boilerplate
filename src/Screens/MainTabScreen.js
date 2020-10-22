5b6da580ae46b1df30c9c59feca2056e61a0bf27
lorenz199x <lorenzorbita@gmail.com>
2020-10-22 19:50:35 +0800

    ProfileView
    - Add Profile Screen
    - Add Access to Ca,era and Gallery
    - Add Share to other App

--- src/Screens/MainTabScreen.js
+++ src/Screens/MainTabScreen.js
@@ -1,72 +1,78 @@
 import React from 'react'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/Ionicons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { useTheme, Avatar } from 'react-native-paper';
+import {View} from 'react-native-animatable';
 
 import HomeScreen from './HomeScreen';
 import DetailsScreen from './DetailsScreen';
 import ExplorerScreen from './ExploreScreen';
 import ProfileScreen from './ProfileScreen';
+import EditProfileScreen from './EditProfileScreen';
 
 const HomeStack = createStackNavigator();
 const DetailsStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
 const MainTabScreen = () => (
   <Tab.Navigator
-      initialRouteName="Home"
-      activeColor="#fff"
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeStackScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarColor: '#009387',
-          tabBarIcon: ({ color }) => (
-            <Icon name="home-outline" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={DetailsStackScreen}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarColor: '#1f65ff',
-          tabBarIcon: ({ color }) => (
-            <Icon name="notifications-outline" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarColor: '#694fad',
-          tabBarIcon: ({ color }) => (
-            <Icon name="person-outline" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Explore"
-        component={ExplorerScreen}
-        options={{
-          tabBarLabel: 'Explore',
-          tabBarColor: '#d02868',
-          tabBarIcon: ({ color }) => (
-            <Icon name="aperture-outline" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    initialRouteName="Home"
+    activeColor="#fff"
+  >
+    <Tab.Screen
+      name="Home"
+      component={HomeStackScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarColor: '#009387',
+        tabBarIcon: ({ color }) => (
+          <Icon name="home-outline" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Notifications"
+      component={DetailsStackScreen}
+      options={{
+        tabBarLabel: 'Updates',
+        tabBarColor: '#1f65ff',
+        tabBarIcon: ({ color }) => (
+          <Icon name="notifications-outline" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileStackScreen}
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarColor: '#694fad',
+        tabBarIcon: ({ color }) => (
+          <Icon name="person-outline" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Explore"
+      component={ExplorerScreen}
+      options={{
+        tabBarLabel: 'Explore',
+        tabBarColor: '#d02868',
+        tabBarIcon: ({ color }) => (
+          <Icon name="aperture-outline" color={color} size={26} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
 )
 
 export default MainTabScreen;
 
-const HomeStackScreen = ({navigation}) => {
+const HomeStackScreen = ({ navigation }) => {
   return (
     <HomeStack.Navigator
       screenOptions={{
@@ -96,7 +102,7 @@ const HomeStackScreen = ({navigation}) => {
   );
 };
 
-const DetailsStackScreen = ({navigation}) => (
+const DetailsStackScreen = ({ navigation }) => (
   <DetailsStack.Navigator
     screenOptions={{
       headerStyle: {
@@ -122,3 +128,56 @@ const DetailsStackScreen = ({navigation}) => (
     />
   </DetailsStack.Navigator>
 );
+
+
+const ProfileStackScreen = ({ navigation }) => {
+  const { colors } = useTheme();
+  return (
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+          shadowColor: colors.background, // iOS
+          elevation: 0, // Android
+        },
+        headerTintColor: colors.text,
+      }}>
+      <ProfileStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: '',
+          headerLeft: () => (
+            <View style={{ marginLeft: 10 }}>
+              <Icon.Button
+                name="ios-menu"
+                size={25}
+                backgroundColor={colors.background}
+                color={colors.text}
+                onPress={() => navigation.openDrawer()}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <View style={{ marginRight: 10 }}>
+              <MaterialCommunityIcons.Button
+                name="account-edit"
+                size={25}
+                backgroundColor={colors.background}
+                color={colors.text}
+                onPress={() => navigation.navigate('EditProfile')}
+              />
+            </View>
+          ),
+        }}
+      />
+      <ProfileStack.Screen
+        name="EditProfile"
+        options={{
+          title: 'Edit Profile',
+        }}
+        component={EditProfileScreen}
+      />
+    </ProfileStack.Navigator>
+  )
+};
